ea36e3ed700d2b63b04d1d63dd7f3cdef4056a03
tohutohu <tohu.soy@gmail.com>
2022-10-18 20:00:31 +0900

    6. 商品を追加できるようにする

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -2,7 +2,9 @@ import Item from "./Item";
 import { useState } from "react";
 
 const ItemList = () => {
-  const [items] = useState([
+  const [itemName, setItemName] = useState("");
+  const [itemPrice, setItemPrice] = useState(0);
+  const [items, setItems] = useState([
     { name: "りんご", price: 300 },
     { name: "バナナ", price: 200 },
     { name: "ぶどう", price: 2000 },
@@ -21,6 +23,36 @@ const ItemList = () => {
           );
         })}
       </ul>
+      <div>
+        <label htmlFor="itemName">
+          商品名：
+          <input
+            type="text"
+            id="itemName"
+            onChange={(e) => setItemName(e.target.value)}
+            value={itemName}
+          />
+        </label>
+        <label htmlFor="itemPrice">
+          価格：
+          <input
+            type="number"
+            id="itemPrice"
+            onChange={(e) => setItemPrice(e.target.value)}
+            value={itemPrice}
+          />
+        </label>
+        <button
+          onClick={() => {
+            setItems((items) => [
+              ...items,
+              { name: itemName, price: itemPrice },
+            ]);
+          }}
+        >
+          追加
+        </button>
+      </div>
     </div>
   );
 };
